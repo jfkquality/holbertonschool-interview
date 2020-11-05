1d4bf949fe92869c7a67bb22bb1f959b67ab9892
jfkquality <jfkquality@gmail.com>
2020-11-05 05:41:25 +0000

    455. t.0. Bugfix. Add binary tree print to header file.

--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -24,7 +24,9 @@ typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
 
 avl_t *sorted_array_to_avl(int *array, size_t size);
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
+/* static int print_t(const binary_tree_t *tree, int offset, int depth, char **s); */
 avl_t *create_avl(avl_t *parent, int num);
+/* static size_t _height(const binary_tree_t *tree); */
+void binary_tree_print(const binary_tree_t *tree);
 
 #endif  /* _BINARY_TREE */
