57df88e982853b97af095cfbd2bac014d8861cfe
jfkquality <jfkquality@gmail.com>
2020-11-12 07:18:54 +0000

    461. t.0. Bugfix: Fix 'checked at' after range found. fix not in list.

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -17,21 +17,34 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 
 	if (list == NULL || !value)
 		return (NULL);
+	skip = skip->express;
 	for (node = skip; node; node = node->express)
 	{
+		printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
 		if (value < node->n)
 		{
 			printf("Value found between indexes [%lu] and [%lu]\n",
 			       list->index, node->index);
+
 			while (list->index <= node->index)
 			{
-				printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
+				printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
 				if (value == list->n)
 					return (list);
 				list = list->next;
 			}
 		}
-		printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
+		list = node;
+	}
+	skip = list;
+	while (skip->next)
+		skip = skip->next;
+	printf("Value found between indexes [%lu] and [%lu]\n",
+	       list->index, skip->index);
+	while (list)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
+		list = list->next;
 	}
 	return (NULL);
 }
