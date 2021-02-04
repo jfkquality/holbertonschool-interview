243eb1d4a92f3b781e55f12f9b1d3b78d907c2dc
jfkquality <jfkquality@gmail.com>
2021-02-03 20:22:21 -0800

    503. t.0. [Init commit]

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,14 +3,12 @@
 
 def rotate_2d_matrix(matrix):
     """ Rotate 2D matrikx cw """
-    print(len(matrix))
 
-    size = len(matrix)
-
-    for row, vals in enumerate(matrix):
-        matrix.insert(row, vals[row])
-        print(matrix)
-        # col 0 becomes row 0
-        # col 1 becomes row 1
-        # col x becomes row x
-        # for col, val in enumerate(row);
+    size = len(matrix[0])
+    for i in range(size // 2):
+        for j in range(i, size - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[size - 1 - j][i]
+            matrix[size - 1 - j][i] = matrix[size - 1 - i][size - 1 - j]
+            matrix[size - 1 - i][size - 1 - j] = matrix[j][size - 1 - i]
+            matrix[j][size - 1 - i] = temp
