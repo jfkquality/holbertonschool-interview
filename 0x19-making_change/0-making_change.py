74ee6abbb8209c10a142a959c23d1af85e9c16fa
jfkquality <jfkquality@gmail.com>
2021-02-25 03:53:45 -0500

    492. t.0. [Bugfix]. Account for non-consecutive coin solutions.

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -12,16 +12,45 @@ def makeChange(coins, total):
     if len(coins) <= 0:
         return -1
 
-    sc = sorted(coins)
-    sc = sc[::-1]
+    coinsList = sorted(coins, reverse=True)
+    # print(coins, total)
+    # print(sorted_c, total)
+    # sorted_c = sorted_c[::-1]
 
+    count = 0
     num = 0
+    coinIdx = 0
     running = total
-    for c in sc:
-        while (running - c >= 0):
-            running = running - c
-            num = num + 1
+    temp = total
+    lastIdx = len(coins) -1
 
-    if running != 0 and running - sc[-1] < 0:
-        return -1
-    return num
+    # for coin in coinsList:
+    while coinIdx < len(coinsList):
+        num = total // coinsList[coinIdx]
+        count += num
+        # total -= coinsList[coinIdx] * num
+        temp -= coinsList[coinIdx] * num
+        # print("first temp ", temp)
+
+        if coinIdx == lastIdx - 1 and temp != 0:
+            # print("next coin list value ", coinsList[coinIdx + 1])
+            if temp < coinsList[coinIdx + 1]: # and temp != 0: # and coindIdx + 1 < len(coinsList) - 1:
+                count -= num
+                temp = total
+                # print("temp ", temp)
+        total = temp
+        # print("total ", total)
+        coinIdx += 1
+
+        if total == 0:
+            return count
+    return -1
+
+    # for c in sorted_c:
+    #     while (running - c >= 0):
+    #         running = running - c
+    #         num = num + 1
+
+    # if running != 0 and running - sorted_c[-1] < 0:
+    #     return -1
+    # return num
