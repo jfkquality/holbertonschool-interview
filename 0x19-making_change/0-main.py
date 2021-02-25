b7161990557e1cc1cf415664fdaefa0c7b5a5375
jfkquality <jfkquality@gmail.com>
2021-02-25 00:05:38 -0500

    492. t.0. [Bugfix?]. Remove added lines from 0-main.

--- 0x19-making_change/0-main.py
+++ 0x19-making_change/0-main.py
@@ -8,9 +8,3 @@ makeChange = __import__('0-making_change').makeChange
 print(makeChange([1, 2, 25], 37))
 
 print(makeChange([1256, 54, 48, 16, 102], 1453))
-
-print(makeChange([1, 2, 25, 36, 40], 37))
-
-print(makeChange([1, 2, 25, 37], 50))
-
-print(makeChange([2, 4, 26, 37], 50))
