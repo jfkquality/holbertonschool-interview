40cd2f130c4c172f92d7066d4d4cc4eaa1ce5086
jfkquality <jfkquality@gmail.com>
2021-04-07 14:27:22 -0400

    500. t.0. [Init commit].

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+0. Pascal's Triangle
+"""
+
+def pascal_triangle(n):
+    """
+    Create Pascal's Triangle
+    """
+
+    triangle = [[1]]
+    row = 1
+    while row < n:
+        newRow = [1]
+        idx = 1
+        while idx < row:
+            newRow.append(triangle[row - 1][idx - 1] +
+                        triangle[row - 1][idx])
+            idx += 1
+        newRow.append(1)
+        triangle.append(newRow)
+        row += 1
+    return [triangle]
