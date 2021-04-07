d198d1faec0ae9bacb9119476d1ca25b5d4dfb82
jfkquality <jfkquality@gmail.com>
2021-04-07 14:30:34 -0400

    500. t.0. [Bugfix]. Linter errors

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 0. Pascal's Triangle
 """
 
+
 def pascal_triangle(n):
     """
     Create Pascal's Triangle
@@ -15,7 +16,7 @@ def pascal_triangle(n):
         idx = 1
         while idx < row:
             newRow.append(triangle[row - 1][idx - 1] +
-                        triangle[row - 1][idx])
+                          triangle[row - 1][idx])
             idx += 1
         newRow.append(1)
         triangle.append(newRow)
