0a199486e2eaa01a8df9c3b828697ab5d76ec49a
jfkquality <jfkquality@gmail.com>
2021-04-22 01:09:54 -0400

    506. t.0. [Bugfix]. Include stdlib.h in substring.h

--- 0x21-substring/substring.h
+++ 0x21-substring/substring.h
@@ -1,10 +1,12 @@
 #ifndef _SUBSTRING_H
 #define _SUBSTRING_H
 
+#include <stdlib.h>
+
 int *find_substring(char const *s, char const **words, int nb_words, int *n);
 int not_in(int *arr, int len, int q);
 int str_match(char const *str, char const *word);
 int full_match(char const *s, char const **words, int nb_words, int word_len);
 
 
-#endif _SUBSTRING_H;
+#endif /* _SUBSTRING_H; */
