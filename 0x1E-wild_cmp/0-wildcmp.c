ff5071fc1655b11f4e22e1b580634d6a93b339db
jfkquality <jfkquality@gmail.com>
2022-03-12 09:51:49 -0500

    502. Wild Compare.. [Bugfix]. Linter errors.

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -11,23 +11,23 @@
 
 int wildcmp(char *s1, char *s2)
 {
-        /* If we reach the end of both strings, we are done */
-        if (*s1  == '\0' && *s2 == '\0')
-                return (1);
+	/* If we reach the end of both strings, we are done */
+	if (*s1  == '\0' && *s2 == '\0')
+		return (1);
 
-        /* Make sure that the characters after * are present */
-        /* in second string. */
-        if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
-                return (0);
+	/* Make sure that the characters after * are present */
+	/* in second string. */
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
 
-        /* If characters match, go to next one */
-        if (*s2 == *s1)
-                return (wildcmp(s1 + 1, s2 + 1));
+	/* If characters match, go to next one */
+	if (*s2 == *s1)
+		return (wildcmp(s1 + 1, s2 + 1));
 
-        /* If there is a *, then there are two possibilities */
-        /* a) We consider current character of second string */
-        /* b) We ignore current character of second string. */
-        if (*s2 == '*')
-                return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
-        return (0);
+	/* If there is a *, then there are two possibilities */
+	/* a) We consider current character of second string */
+	/* b) We ignore current character of second string. */
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0);
 }
