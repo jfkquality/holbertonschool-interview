1ed74626ff6c4a3bb9dba543c2d95b14bb728c7f
jfkquality <jfkquality@gmail.com>
2022-04-25 12:11:07 -0400

    500. 0x1F. Pascal's Triangle [Init commit].

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+""" Pascal's Triangle """
+
+
+def pascal_triangle(n):
+    """ returns a list of lists of integers """
+    list = []
+
+    for i in range(1, n + 1, 1):
+        row = [1] * i
+        for j in range(i):
+            if j != 0 and j != i - 1:
+                row[j] = list[i - 2][j] + list[i - 2][j - 1]
+        list.append(row)
+
+    return list
