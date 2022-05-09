22627a4097e856ffee887415683fd9861967a7f9
jfkquality <jfkquality@gmail.com>
2022-05-08 23:10:29 -0400

    0x21 [Bugfix] Add documention to header file.

--- 0x21-substring/substring.h
+++ 0x21-substring/substring.h
@@ -4,6 +4,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * enum word_status - progress of a word in the current substring search
+ * @WORD_DEFAULT: word not confirmed found or not found
+ * @WORD_FOUND: word found in the substring
+ * @WORD_NOT: word can't be at this point in the substring
+ */
 enum word_status
 {
 	WORD_DEFAULT, WORD_FOUND, WORD_NOT
