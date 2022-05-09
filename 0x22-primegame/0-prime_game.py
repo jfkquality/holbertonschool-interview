1a5868b0945fff9d921410c77ecda8cb032c846b
jfkquality <jfkquality@gmail.com>
2022-05-09 01:23:14 -0400

    0x22. Prime Game. {Refactor]

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -1,32 +1,67 @@
 #!/usr/bin/python3
-"""Prime Game"""
+"""This modual contains the function used in Task 0"""
 
 
-def isPrime(num):
-    """determines if a number is prime"""
-    if num > 1:
-        for i in range(2, int(num / 2) + 1):
-            if (num % i) == 0:
-                return False
-        else:
-            return True
-    else:
-        return False
+def is_prime(n):
+    """
+      this function checks if the number is a prime
+    """
+    for i in range(int(n**(1 / 2)), int(n / 2)):
+        if not (n % i):
+            return 0
+    return 1
+
+
+def find_multiples(array, num):
+    """
+      this function removes the number and any
+      of the multiples from the array
+    """
+    for i in array:
+        if i % num == 0:
+            if (num == i and is_prime(i)):
+                continue
+            array.remove(i)
+
+
+def game(array, winner):
+    """
+      this function recursivly plays the prime game
+    """
+    for _ in array:
+        winner = 'Maria' if winner == 'Ben' else 'Ben'
+    return winner
+
+
+def list_maker(array):
+    """
+      this function recursivly plays the prime game
+    """
+    i = 0
+    while i < len(array):
+        value = array[i]
+        find_multiples(array, value)
+        i += 1
+
+    return array
+
+
+def get_relevant_primes(primes, num):
+    return [i for i in primes if i <= num]
 
 
 def isWinner(x, nums):
-    """Maria and Ben take turns choosing prime numbers"""
-    winner = 0
-    for round in range(x):
-        prime = 1
-        for n in range(1, nums[round] + 1):
-            if isPrime(n):
-                prime += 1
-        winner += 1 if prime % 2 == 1 else -1
-    if winner == 0:
+    """
+      This function is the main function of the prime game,
+      it returns who one.
+    """
+    winner = []
+    if x < 1:
         return None
-    elif winner > 0:
-        return "Ben"
-    else:
-        return "Mariia"
-#    return None if winner == 0 else "Ben" if winner > 0 else "Maria"
+    primes = list_maker(list(range(2, max(nums) + 1)))
+
+    for i in nums:
+        winner.append(game(get_relevant_primes(primes, i), 'Ben'))
+    if winner.count('Ben') == winner.count('Maria'):
+        return None
+    return 'Ben' if winner.count('Ben') > winner.count('Maria') else 'Maria'
